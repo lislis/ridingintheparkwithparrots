56a02f6f5aa53f910ca0f9530b7ca22cffc65ac3
lislis <mail@lislis.de>
2023-09-22 17:39:28 +0200

    split into plugins

---
+++ src/player.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_player);
+    }
+}
+
+fn spawn_player(mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>) {
+        let player = PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube::new(1.0))),
+            material: materials.add(Color::BLUE.into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        };
+        commands.spawn(player);
+}
